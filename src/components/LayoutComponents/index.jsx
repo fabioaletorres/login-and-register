da9cc764bc75e199671f86227a2c9590f37909ff
fabioaletorres <fabioalessandrotorres@gmail.com>
2023-01-10 00:03:01 -0300

    cadastro

---
+++ src/components/LayoutComponents/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+export const LayoutComponents = (props) => {
+  return (
+    <div className="container">
+      <div className="container-layout">
+        <div className="wrap-layout">{props.children}</div>
+      </div>
+    </div>
+  );
+};
